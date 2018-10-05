2010b14a67909d7ae27e8f03a419ba97ba37e50d
noahmiller123 <43760943+noahmiller123@users.noreply.github.com>
2018-10-05 11:59:55 -0700

    Add files via upload

---
+++ LootScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootScript : MonoBehaviour {
+    [System.Serializable]
+    public class DropCurrency
+    {
+        public string name;
+        public GameObject item;
+        public int dropRarity;
+    }
+
+    public List<DropCurrency> LootTable = new List<DropCurrency>();
+    public int dropChance;
+    
+
+    public void calculateloot()
+    {
+        int calc_dropChance = Random.Range(0, 101);
+
+        if(calc_dropChance > dropChance)
+        {
+            Debug.Log("No Loot for me");
+            return;
+        }
+
+        if(calc_dropChance <= dropChance)
+        {
+            int itemWeight = 0;
+
+            for(int i = 0; i < LootTable.Count; i++)
+            {
+                itemWeight += LootTable[i].dropRarity;
+            }
+            Debug.Log("itemWeight=" + itemWeight);//135
+
+            int randomValue = Random.Range(0, itemWeight);//80
+
+            for (int Coin = 0; Coin < LootTable.Count; Coin++)
+            {
+             
+                if (randomValue <= LootTable[Coin].dropRarity)
+                {
+                    Instantiate(LootTable[Coin].item,transform.position,Quaternion.identity);
+                    return;
+                }
+                randomValue -= LootTable[Coin].dropRarity;
+                Debug.Log("Random Value decreased" + randomValue);
+            }
+        }
+    }
+}
