ed49accd32f43e6a4e5336e5ce9d169b115775e9
noahmiller123 <43760943+noahmiller123@users.noreply.github.com>
2018-10-05 12:25:06 -0700

    Add files via upload

---
+++ HP.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour
+{
+
+    public int health = 10;
+    public Text healthText;
+    public Slider healthBar;
+    
+
+    void Start()
+    {
+        health--;
+        healthText.GetComponent<Text>().text =
+            "Health" + health;
+        healthBar.GetComponent<Slider>().value = health;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            healthText.GetComponent<Text>().text =
+              "Health" + health;
+            healthBar.GetComponent<Slider>().value = health;
+            health--;
+            if (health <= 0)
+                SceneManager.LoadScene("LoseScreen");
+
+                
+                
+        
+        }
+
+        if (collision.gameObject.tag == "heal")
+        {
+            health += 5;
+            Destroy(collision.gameObject);
+        }
+        
+    }
+
+
+}
