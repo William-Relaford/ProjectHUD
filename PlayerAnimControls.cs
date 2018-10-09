fbddc412b4d077235002eb02b0841577b292bdb8
noahmiller123 <43760943+noahmiller123@users.noreply.github.com>
2018-10-09 12:04:09 -0700

    Add files via upload

---
+++ PlayerAnimControls.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimControls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        if(Mathf.Abs(x) > Mathf.Abs(y))
+        {
+            GetComponent<Animator>().SetFloat("x", x);
+            GetComponent<Animator>().SetFloat("y", 0);
+        }else if(Mathf.Abs(x) < Mathf.Abs(y))
+        {
+            GetComponent<Animator>().SetFloat("x", 0);
+            GetComponent<Animator>().SetFloat("y", y);
+        }
+        if(x == 0 && y == 0)
+        {
+            GetComponent<Animator>().SetBool("idle", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("idle", false);
+        }
+        /*if(x != 0)
+        {
+            GetComponent<Animator>().SetFloat("x", x);
+            GetComponent<Animator>().SetBool("idle", false);
+        }
+        else if(y != 0)
+        {
+            GetComponent<Animator>().SetFloat("y", y);
+            GetComponent<Animator>().SetBool("idle", false);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("idle", true);
+        }*/
+		
+	}
+}
